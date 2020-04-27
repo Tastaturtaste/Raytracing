434e290bd94a7c1b0052e605dd383883ea894177
Joshua Möllers <joshua.moellers@gmx.net>
2020-04-27 04:41:21 +0200

    modified intersect method of triangle with bitset defaulted to zero

--- Raytracer/primitives/Triangle.cpp
+++ Raytracer/primitives/Triangle.cpp
@@ -16,13 +16,13 @@ std::optional<IntersectionTrace> Triangle::intersect(const Ray& r) const {
 	if (t < constants::EPS) return {};	// Reyorigin behind triangle
 	const Vec3 hitPoint = r.positionAlong(t);
 	// check for each leg if hitpoint is on the left. Vertices are expected to be in mathmatical order / against the clock
-	std::bitset<3> isRight(3);
+	std::bitset<3> isRight(0);
 	const Vec3 pv0 = hitPoint - vertices_[0];
 	const Vec3 pv1 = hitPoint - vertices_[1];
 	const Vec3 pv2 = hitPoint - vertices_[2];
 	isRight.set(0, N.dot(leg<0, 1>().cross(pv0)) < 0);
 	isRight.set(1, N.dot(leg<1, 2>().cross(pv1)) < 0);
 	isRight.set(2, N.dot(leg<2, 0>().cross(pv2)) < 0);
-	if (isRight.all()) return {};
+	if (isRight.none()) return {};
 	return IntersectionTrace(false, normal_, hitPoint, t, material());
 }
